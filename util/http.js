7c8acf2173a7a3a4dc14a49c8fcb56d4d418adf7
Michael <michaelmamaye@gmail.com>
2023-02-23 17:50:10 +0300

    added firebase backcend and error handling

---
+++ util/http.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const URL = 'https://expensetracker-a1878-default-rtdb.firebaseio.com/'
+
+
+export async function storeExpense(expenseData) {
+    console.log(expenseData)
+    const response = await axios.post(`${URL}/expenses.json`, expenseData);
+    const id = response.data.name;
+    return id;
+}
+
+export async function fetchExpenses() {
+    const response = await axios.get(`${URL}/expenses.json`);
+
+    const expenses = [];
+
+    for (const key in response.data) {
+        const expenseObj = {
+            id: key,
+            amount: response.data[key].amount,
+            date: new Date(response.data[key].date),
+            description: response.data[key].description
+        };
+        expenses.push(expenseObj);
+    }
+
+    return expenses;
+}
+
+export function updateExpense(id, expenseData) {
+    return axios.put(`${URL}/expenses/${id}.json`, expenseData);
+}
+
+export function deleteExpense(id) {
+    return axios.delete(`${URL}/expenses/${id}.json`);
+}
